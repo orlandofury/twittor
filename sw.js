ac97f9c25740ac48665929e77cf3542d8e887c3a
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-31 17:05:23 -0600

    Quitando el fontawesome all.css para probar el deployment

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE =[
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    
     'css/animate.css',
     'js/libs/jquery.js'
 ];
