b0b4c0d1160ce54a55b1fbea6badfb298da912c8
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-31 17:33:36 -0600

    Modifiaciones para IOS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v2';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inumatable-v1';
 
